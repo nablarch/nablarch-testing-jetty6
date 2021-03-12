eac9afe164c37c9de9ea0507d28523c2bad4f419
mseko <masaya.seko@nifty.ne.jp>
2021-03-12 15:40:28 +0900

    WebConfigのプロパティ名変更対応及び、Content-Typeのassert方法変更対応

--- src/test/java/nablarch/fw/web/HttpServerTest.java
+++ src/test/java/nablarch/fw/web/HttpServerTest.java
@@ -621,7 +621,7 @@ public class HttpServerTest {
         HttpResponse res = server.handle(req, ctx);
 
         assertEquals(200, res.getStatusCode());
-        assertNull(res.getContentType());
+        assertNull(res.getHeader("Content-Type"));
         assertEquals("/app/test.html", req.getRequestPath());
 
         File[] dumpFiles = dumpRoot.listFiles();
@@ -657,7 +657,7 @@ public class HttpServerTest {
     @Test
     public void testHttpMessageDumpBodyEmptyFacilitiesForResponseWithNoBodyEnabledTrue() throws Exception {
         final WebConfig webConfig = new WebConfig();
-        webConfig.setContentTypeForResponseWithNoBodyEnabled(true);
+        webConfig.setAddDefaultContentTypeForNoBodyResponse(true);
         SystemRepository.load(new ObjectLoader() {
             @Override
             public Map<String, Object> load() {
@@ -698,7 +698,7 @@ public class HttpServerTest {
         HttpResponse res = server.handle(req, ctx);
 
         assertEquals(200, res.getStatusCode());
-        assertEquals("text/plain;charset=UTF-8", res.getContentType());
+        assertEquals("text/plain;charset=UTF-8", res.getHeader("Content-Type"));
         assertEquals("/app/test.html", req.getRequestPath());
 
         File[] dumpFiles = dumpRoot.listFiles();
